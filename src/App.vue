28f44971ef69ed653083b0fe4d05a8ee52a3b748
akshzt <akyvzz@gmail.com>
2022-06-12 19:17:26 +0530

    learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ greet }} {{ name }}</div>
+  <div v-text="color"></div>
+  <div v-html="colorHtml"></div>
+  <div v-html="alertRandom"></div>
+  <div v-bind:id="headingId">Heading</div>
+  <button v-bind:disabled="isDisabled">Button</button>
+  <h2 class="underline">Underlined text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Sold Out</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array Conditional Movie</h2>
+  <h2 v-bind:class="{
+    promoted: isPromoted,
+    new: !isSoldOut,
+    'sold-out': isSoldOut,
+  }">
+    Object Conditional Movie
+  </h2>
+
+  <h2 v-bind:style="{
+    color: highlightColor,
+    'font-size': headerSize + 'px',
+  }">Inline Style</h2>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      greet: 'Hello',
+      name: 'SomeName',
+      color: 'Purple',
+      colorHtml: '<b>Red</b>',
+      alertRandom: `<a href="#" onclick="alert('Clicked on this!')">Click here</a>`,
+      headingId: 'heading',
+      isDisabled: true,
+      status: 'danger',
+      isPromoted: true,
+      isSoldOut: true,
+      highlightColor: 'orange',
+      headerSize: 50,
+    };
+  },
+};
 </script>
 
 <style>
@@ -23,4 +57,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
+}
+
 </style>
